416327ee4c2c2face09e736a08ea8979cec68746
Alejandro C. Grant <authorcgrant@gmail.com>
2021-09-10 10:59:16 -0700

    backend has base functionality

--- backend/app.js
+++ backend/app.js
@@ -6,32 +6,34 @@ const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const { environment } = require('./config');
 const routes = require('./routes');
-
 const isProduction = environment === 'production';
 const app = express();
+
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
-app.use(routes);
 
 //? cors enabled for development only
 if (!isProduction) {
     app.use(cors());
-  }
-  //? helmet security set up
-  app.use(helmet({
+}
+//? helmet security set up
+app.use(helmet({
     contentSecurityPolicy: false
-  }));
+}));
 
-  //? _csrf token set and req.csrfToken method created
-  app.use(
+//? _csrf token set and req.csrfToken method created
+app.use(
     csurf({
-      cookie: {
-        secure: isProduction,
-        sameSite: isProduction && "Lax",
-        httpOnly: true,
-      },
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true,
+        },
     })
-  );
+    );
 
-  module.exports = app;
+//? has to run after csurf
+app.use(routes);
+
+module.exports = app;
