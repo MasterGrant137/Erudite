098273fe795c7b9f588b5b46b2ffaacc5ec49c0f
Alejandro C. Grant <authorcgrant@gmail.com>
2021-09-10 12:44:11 -0700

    error handling done

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ const { environment } = require('./config');
 const routes = require('./routes');
 const isProduction = environment === 'production';
 const app = express();
+const { ValidationError } = require('sequelize');
 
 app.use(morgan('dev'));
 app.use(cookieParser());
@@ -35,5 +36,35 @@ app.use(
 
 //? has to run after csurf
 app.use(routes);
+
+//? error handling
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+  });
+
+//? process sequelize errors
+app.use((err, _req, _res, next) => {
+    if (err instanceof ValidationError) {
+      err.errors = err.errors.map((e) => e.message);
+      err.title = 'Validation error';
+    }
+    next(err);
+  });
+
+//? format errors
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack,
+    });
+  });
 
 module.exports = app;
