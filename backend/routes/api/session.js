557b69ba0bdf2b2c351e4e25e673a4cdddb68892
Alejandro C. Grant <authorcgrant@gmail.com>
2021-09-11 17:26:05 -0700

    session user route established

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -29,6 +29,19 @@ router.post(
     }),
   );
 
+  router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        return res.json({
+          user: user.toSafeObject()
+        });
+      } else return res.json({});
+    }
+  );
+
   router.delete(
     '/',
     (_req, res) => {
