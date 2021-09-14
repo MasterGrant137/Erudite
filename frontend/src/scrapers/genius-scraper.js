4e9d6696809417f92485e959ad1951a2cf0ce8f6
Alejandro C. Grant <authorcgrant@gmail.com>
2021-09-14 09:34:02 -0700

    scraper gained base functionality

--- frontend/src/scrapers/genius-scraper.js
+++ frontend/src/scrapers/genius-scraper.js
@@ -1,26 +1,32 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const fs = require('fs');
 const pretty = require('pretty');
 const { geniusClientToken } = require('./genius-auth.json');
 
-const url = 'https://api.genius.com/artists/16775/songs?per_page=10';
+const artist = '137-us'
+//? finding artist by ID
+// const url = `https://api.genius.com/artists/${id}/songs?per_page=10`;
+//? allowing for search by artist
+const url = `https://api.genius.com/search?per_page=20/`;
 // const fltr = '/search?per_page=10&page=';
 
-const artist = '137-us'
 // const searchUrl = `${url}${artist}`
 const searchUrl = `${url}`
 
 
 const geniusFetcher = () => {
-   
+
 console.log(geniusClientToken);
     axios.get(searchUrl, {
         headers: { 'Authorization': `Bearer ${geniusClientToken}` },
-        // data: { 'q': artist }
+        data: { 'q': artist }
     })
     .then(res => {
-        const $ = cheerio.load(res.data);
-        console.log(res.data.response.songs[0].url);
+        // const individualArtist = res.data.response.hits[0].result.primary_artist;
+        const artistSongs = res.data.response.hits
+
+        lyricsScraper()
     })
     .catch(err => {
         console.error(`You have this error bud: ${err}`)
@@ -28,7 +34,8 @@ console.log(geniusClientToken);
 }
 
 
-const geniusScraper = () => {
+const lyricsScraper = () => {
+    const $ = cheerio.load(res.data);
 
 }
 
