d98cfce09c5f50ddc3ff66f9f94127c0b8b439c4
Alejandro C. Grant <authorcgrant@gmail.com>
2021-09-15 17:31:18 -0700

    cleaning regex

--- frontend/src/data-formatting/seeder-functions.js
+++ frontend/src/data-formatting/seeder-functions.js
@@ -1,29 +1,33 @@
 const fs = require('fs');
 // file seeder write route: '../../../backend/db/seeders/file-name'
-// final parameters: metaTitleStr, producer, lyricsDivStr, media, visits, coverArt
+// final parameters: metaTitleStr, producerStr, lyricsDivStr, media, visits, coverArt
 // js Seeding template: `const obj = \`{${body}}\``
 // dbEntry template: const dbEntry = `{${artist},${title},${producer},${body},${media},${visits},${coverArt}},`
 const f1Seeder = (producerStr) => {
     //? regex tier 1
     const nbsp = /&nbsp;/g
-    const ampersand = /&amp;/g
-    const space = /\u0020/
-    const enDash= /\u2013/
+    const amp = /&amp;/g
+    const closedAnchor = /<\/a>/g
+    const space = '\u0020'
+    const ampersand = '\u0026'
+    const enDash = '\u2013'
+    const pipe = '\u007C'
 
     //? regex tier 2
     const brDivRegex = /<br>|<(\/)?div>/g
-    const strayTagsRegex = /(<(?:a|span|\/|).*?>)|(.*?)/g
+    // const strayTagsRegex = /(<(?:a|span|\/|).*?>)|(.*?)/g
+    const strayTagsRegex = /(<(?:a|span|\/|).*?>)|(View All Credits)|(&nbsp;)|(.*?)/g
 
     // //? artist & title
-    // const artistAndTitleArray = producerStr.replace(nbsp, space).split(`${space}${enDash}${space}`);
+    // const artistAndTitleArray = metaTitleStr.replace(nbsp, space).split(`${space}${enDash}${space}`);
     // const artist = artistAndTitleArray[0];
     // const title = artistAndTitleArray[1];
 
     //? body
-    // const body = lyricsDivStr.replace(brDivRegex, '\n').replace(ampersand, '\u0026').replace(strayTagsRegex, '$2');
+    // const body = lyricsDivStr.replace(brDivRegex, '\n').replace(amp, ampersand).replace(strayTagsRegex, '$2');
 
     //? producer
-    const producer = producerStr.replace(nbsp, space).replace(strayTagsRegex, '$1$2\u0020$3')
+    const producer = producerStr.replace(closedAnchor, pipe).replace(strayTagsRegex, '$4');
 
 // console.log(producer);
     fs.writeFile('./scraped-info.js', producer, err => {
