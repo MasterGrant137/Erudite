bd2e086c58ad6f136e4416cdc170636cad5ce84a
Alejandro C. Grant <authorcgrant@gmail.com>
2021-09-18 13:35:21 -0700

    route mapping seemingly complete

--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -19,17 +19,71 @@ router.get('/', asyncHandler(async(req, res) => {
         order: [['visits', 'DESC']],
         limit: 20
     });
-    return res.json(songs);
+    res.json(songs);
 }));
 
 router.get('/:id', asyncHandler(async(req, res) => {
     const song = await Song.findOne({
         where: {
             songID: req.params.id
-         }
+         },
+         include: User
     })
+    res.json(song)
 }))
 
+
+router.get('/:id/comments', asyncHandler(async(req, res) => {
+    const comments = await Comment.findAll({
+        where: {
+            songID: req.params.id
+        },
+        include: User
+    })
+    res.json(comments)
+}))
+
+router.post(':id/comments', validateComment, asyncHandler(async(req, res) => {
+    const { userID, songID, body } = req.body;
+
+    const commentSubmission = await Comment.create({
+        userID,
+        songID,
+        body
+    });
+
+    const { id } = commentSubmission;
+    const comment = await Comment.findByPk(id, {
+        include: User
+    });
+    res.json(comment)
+}))
+
+router.get('/:id/annotations', asyncHandler(async(req, res) => {
+    const annotations = await Annotation.findAll({
+        where: {
+            songID: req.params.id,
+        },
+        include: User
+    })
+    res.json(annotations)
+}))
+
+router.post('/:id/annotations', asyncHandler(async(req, res) => {
+    const { userID, songID, body, startPos, endPos } = req.body;
+    const annotationSubmission = await Annotation.create({
+        userID,
+        songID,
+        body,
+        startPos,
+        endPos
+    })
+    const { id } = annotationSubmission;
+    const annotation = await Annotation.findByPk(id, {
+        include: User
+    })
+    res.json(annotation)
+}))
 
 // router.get('/search', asyncHandler(async(req, res) => {
 //     const query = req.url
