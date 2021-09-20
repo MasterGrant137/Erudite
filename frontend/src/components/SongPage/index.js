7a191973d6e17b6520927561a19117fa4cdce2ad
Alejandro C. Grant <authorcgrant@gmail.com>
2021-09-20 03:15:37 -0700

    splendid styling added, pushing through to the finish line

--- frontend/src/components/SongPage/index.js
+++ frontend/src/components/SongPage/index.js
@@ -11,72 +11,53 @@ export const SongPage = () => {
     const iframeRegex= /(<iframe)|id="(.*?)"|src="(.*?)"|title="(.*?)"(><\/iframe>)/g
     const songParams = useParams(); //* for redirection to edit page verification
 
-    console.log(songParams.title);
-
    useEffect(() => {
         dispatch(songPage(songParams.title));
     }, [dispatch])
 
-//     const songSelector = useSelector(state => {
-//         return state.song;
-//     })
+    const songSelector = useSelector(state => {
+        return state.songs;
+    })
 
-//     console.log(songSelector);
+    const song = Object.values(songSelector)[0];
 
-//     const song = Object.values(songSelector)[0];
-
-    const [visibility, setVisibility] = useState('hidden-mySongs-info');
-    const [size, setSize] = useState('big-mySongs-lyrics');
+    const [size, setSize] = useState('big-song-page-lyrics');
 
     return (
-        <div>This is HIT</div>
-        // <div id='mySongs-page-container'>
-        //     <div id='mySongs-bg-color-wrapper'>
-        //         <div id='mySongs-headers-container'>
-        //             <div id='mySongs-page-header'>Erudite</div>
-        //             <div id='mySongs-header'>Edit Song</div>
-        //         </div>
-        //         <div id='mySongs-carousel'>
-        //             <div key={song?.media.replace(iframeRegex, '$2')}>
-        //                 <div
-        //                     key={song?.media.replace(iframeRegex, '$2')}
-        //                     id={size}
-        //                     className='mySongs-lyrics'
-        //                     onMouseOver={() => {
-        //                         setVisibility('visible-mySongs-info');
-        //                         setSize('small-mySongs-lyrics');
-        //                     }}
-        //                     onMouseOut={() => {
-        //                         setVisibility('hidden-mySongs-info');
-        //                         setSize('big-mySongs-lyrics');
-        //                     }}
-        //                 >
-        //                     <input id='mySongs-song-title'  value={song?.title} />
-        //                     <input id='mySongs-song-artist' value={song?.artist} />
-        //                     <input id='mySongs-song-producer' value={song?.producer} />
-        //                     <input id='mySongs-song-media' value={song?.media} />
-        //                     <input id='mySongs-song-coverArt' value={song?.coverArt} />
-        //                     <textarea id='mySongs-lyrics-field' value={song?.body} />
-        //             </div>
-        //                 <div
-        //                    id={visibility}
-        //                     onMouseOver={() => {
-        //                        setVisibility('visible-mySongs-info');
-        //                        setSize('small-mySongs-lyrics');
-        //                    }}
-        //                    onMouseOut={() => {
-        //                        setVisibility('hidden-mySongs-info');
-        //                        setSize('big-mySongs-lyrics')
-        //                    }}
-        //                 >
-        //                    <span>Visits: {`${song?.visits}`}</span> <br />
-        //                    <span>Created: {`${song?.createdAt}`}</span> <br />
-        //                    <span>Updated: {`${song?.updatedAt}`}</span>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </div>
+        <div id='song-page-container'>
+            <div id='sp-background-color-wrapper'>
+                <div id='song-page-headers-container'>
+                    <span id='song-page-header'>Erudite</span>
+                    <span id='song-page-title'>{song?.title}</span>
+                </div>
+                <div id='sp-inner-container'>
+                    <div id='song-page-lyrics'>
+                         <textarea
+                             id='song-page-lyrics'
+                             key={song?.id}
+                             value={song?.body}
+                             title={song?.title}
+                             disabled
+                         />
+                    </div>
+                    <div className='song-page-video' id={size}>
+                        <iframe
+                            id={size}
+                            className='home-video'
+                            src={song?.media.replace(iframeRegex,'$3')}
+                            title={song?.title}
+                            allow='fullscreen'
+                            onMouseOver={() => {
+                                setSize('small-home-video');
+                            }}
+                            onMouseOut={() => {
+                                setSize('big-home-video');
+                            }}
+                         />
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
